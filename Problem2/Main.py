e04f428a59b6722ee61de9f0018c956786fe9072
Qayyum <qayyumblazer@gmail.com>
2021-05-21 22:13:27 +0800

    Improvment

--- Problem2/Main.py
+++ Problem2/Main.py
@@ -1,6 +1,13 @@
-import urllib.request, urllib.error, urllib.parse, obo, Graph
+import obo
 import plotly.graph_objects as go
 import numpy as np
+
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += ele+' '
+    return str1
+
 file = open('GDEX.txt', encoding="utf8")
 text=file.read()
 file.close()
@@ -8,6 +15,7 @@ file.close()
 
 fullwordlist = obo.stripNonAlphaNum(text)
 wordlist = obo.removeStopwords(fullwordlist, obo.stopwords)
+wordString = listToString(wordlist)
 dictionary = obo.wordListToFreqDict(wordlist)
 sorteddict = obo.sortFreqDict(dictionary)
 
@@ -19,13 +27,8 @@ N = 100000
 t = list(dictionary.keys())
 y = list(dictionary.values())
 fig = go.Figure(data=go.Scatter(x=t, y=y, mode='markers'))
-fig.update_layout(
-    xaxis = dict(
-        tickmode = 'array',
-        tickvals = list(dictionary.keys()),
-        ticktext = list(dictionary.keys()),
-    )
-)
+
 
 fig.show()
+
 
