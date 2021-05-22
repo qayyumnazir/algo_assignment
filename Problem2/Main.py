84572a214864a061637705b6ef7d62270a536fdc
Qayyum <qayyumblazer@gmail.com>
2021-05-22 15:41:22 +0800

    Positive Nega

--- Problem2/Main.py
+++ Problem2/Main.py
@@ -8,27 +8,62 @@ def listToString(s):
         str1 += ele+' '
     return str1
 
+file = open('positive.txt', encoding="utf8")
+positive_Word=file.read()
+file.close()
+file = open('negative.txt', encoding="utf8")
+negative_Word=file.read()
+file.close()
+
+
+
+posiwordlist = positive_Word.split(", ")
+negawordlist = negative_Word.split(", ")
+
 file = open('GDEX.txt', encoding="utf8")
 text=file.read()
 file.close()
 
 
 fullwordlist = obo.stripNonAlphaNum(text)
-wordlist = obo.removeStopwords(fullwordlist, obo.stopwords)
+wordlist=obo.rabinKarp1(fullwordlist, obo.stopwords,101)
+wordlist1=obo.rabinKarp2(wordlist, posiwordlist,101)
+wordlist2=obo.rabinKarp2(wordlist,negawordlist,101)
+
+print(list(wordlist))
+print(list(wordlist1))
+print(list(wordlist2))
+
+
+
 wordString = listToString(wordlist)
 dictionary = obo.wordListToFreqDict(wordlist)
+dictionaryposi = obo.wordListToFreqDict(wordlist1)
+dictionarynega = obo.wordListToFreqDict(wordlist2)
 sorteddict = obo.sortFreqDict(dictionary)
+sorteddictposi = obo.sortFreqDict(dictionaryposi)
+sorteddictnega = obo.sortFreqDict(dictionarynega)
 
-for s in sorteddict: print(str(s))
-
-print("w")
+for s in sorteddictposi: print(str(s))
 
 N = 100000
 t = list(dictionary.keys())
 y = list(dictionary.values())
-fig = go.Figure(data=go.Scatter(x=t, y=y, mode='markers'))
+fig1 = go.Figure(data=go.Scatter(x=t, y=y, mode='markers'))
+fig1.show()
 
+x1 = wordlist1
+x2 = wordlist2
+
+fig = go.Figure()
+fig.add_trace(go.Histogram(histfunc="sum",x=x1, name="Positive Word"))
+fig.add_trace(go.Histogram(histfunc="sum",x=x2, name="Negative Word"))
 
 fig.show()
+
+
+
+
+
 
 
